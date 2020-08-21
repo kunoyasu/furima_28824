ec9a21382bc8719ae9cb3213a444e73293896645
kunoyasu <kunoyasuyuki@yahoo.co.jp>
2020-08-21 20:13:16 +0900

    commit2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show]
+
   def index
+  #  @item = Items.all
+  end
+
+  def new
+    @item = Items.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
 
   devise_for :users
-root "items#index"
-resources :users, only: [:edit, :update]
+  root "items#index"
+  resources :users, only: [:edit, :update]
 
-end
+  resources :items
+end 
