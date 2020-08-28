81157418db9343dca42c57c4987af20b54e3fc72
kunoyasu <68989986+kunoyasu@users.noreply.github.com>
2020-08-28 09:16:00 +0900

    修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -24,23 +24,23 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     redirect_to item_path(@item) if @item.update(item_params)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :image, :price, :introduction, :category_id, :condition_id, :shipping_charge_id, :shipping_region_id, :estimated_shipping_date_id).merge(user_id: current_user.id)
-    # params.require(:item).permit{"item" => { "name"=>"ゴリラ","image"=>"i","introducion"=>"ゴリラです","category_id"=>1,"condition_id"=>1 ,"comment"=>"test","shipping_charge_id"=>"1" }
   end
 
   def update_params
