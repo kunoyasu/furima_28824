6d6f811f265c9d2253456b91e3f2012286bb0b41
kunoyasu <68989986+kunoyasu@users.noreply.github.com>
2020-08-28 12:36:12 +0900

    修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to("/")
+    if
+     @item.destroy
+     redirect_to root_path
+    else
+     redirect_to item_path(@item.id) 
+    end
   end
 
   private
