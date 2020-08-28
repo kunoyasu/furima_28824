726bee8811917cdd65f9f26392c21344e57c7ab6
kunoyasu <68989986+kunoyasu@users.noreply.github.com>
2020-08-28 12:17:14 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -34,8 +34,13 @@ class ItemsController < ApplicationController
      @item.update(item_params)
      redirect_to item_path(@item) 
     else
-     render "edit"
+     render 'edit'
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to("/")
   end
 
   private
